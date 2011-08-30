62662ec5f445b73dab5cc784585802c8db501462
Thomas Steiner <steiner.thomas@gmail.com>
2011-08-30 18:33:57 +0200

    Added timestamp support (incomplete).

--- search.js
+++ search.js
@@ -68,8 +68,35 @@ function search(req, res, next) {
         });
         reply.on('end', function() {   
           response = JSON.parse(response);
+          var results = [];
+          if (response.data.length) {
+            var items = response.data;
+            for (var i = 0, len = items.length; i < len; i++) {
+              var item = items[i];
+              if (item.type !== 'photo' && item.type !== 'video') {
+                continue;
+              }
+              results.push({
+                /*
+                url: 'https://www.facebook.com/permalink.php?story_fbid=' + 
+                    item.id.split(/_/)[1] + '&id=' + item.from.id,
+                */
+                url: item.type === 'video' ?
+                    item.source :
+                    item.picture,
+                message:
+                    (item.name ? item.name : '') +
+                    (item.caption ? '. ' + item.caption : '') +
+                    (item.description ? '. ' + item.description : '') +
+                    (item.message ? '. ' + item.message : ''),
+                user: 'https://www.facebook.com/profile.php?id=' + item.from.id,
+                type: item.type,
+                timestamp: Date.parse(item.created_time)
+              });
+            }
+          }
           var output = {
-            result: response,
+            result: results,
             source: currentService
           };
           sendResults(output, currentService, requestId);
@@ -81,7 +108,7 @@ function search(req, res, next) {
     twitter: function(requestId) {
       var currentService = 'twitter';         
       var params = {
-        q: query + ' AND ' + '(yfrog.com OR instagr.am OR flic.kr OR moby.to OR youtu.be OR twitpic.com)'
+        q: query + ' AND ' + '(yfrog.com OR instagr.am OR flic.kr OR moby.to OR youtu.be OR twitpic.com) -"RT "'
       };
       params = querystring.stringify(params);
       var options = {
@@ -106,8 +133,23 @@ function search(req, res, next) {
         });
         reply.on('end', function() {   
           response = JSON.parse(response);
+          var results = [];
+          if (response.results.length) {
+            var items = response.results;
+            for (var i = 0, len = items.length; i < len; i++) {
+              var item = items[i];
+              results.push({
+                url: 'http://twitter.com/' +
+                    item.from_user + '/status/' + item.id,
+                message: item.text,
+                user: 'http://twitter.com/' + item.from_user,
+                type: 'micropost',
+                timestamp: Date.parse(item.created_at)
+              });
+            }
+          }
           var output = {
-            result: response,
+            result: results,
             source: currentService
           };
           sendResults(output, currentService, requestId);
@@ -126,7 +168,7 @@ function search(req, res, next) {
         host: 'api.instagram.com',
         port: 443,
         path: '/v1/tags/' +
-            query.replace(/\s*/g, '').toLowerCase() +
+            query.replace(/\s*/g, '').replace(/\W*/g, '').toLowerCase() +
             '/media/recent?' + params,
         headers: {
           "Accept": "application/json, text/javascript, */*",
@@ -146,8 +188,22 @@ function search(req, res, next) {
         });
         reply.on('end', function() {   
           response = JSON.parse(response);
+          var results = [];
+          if (response.data.length) {
+            var items = response.data;
+            for (var i = 0, len = items.length; i < len; i++) {
+              var item = items[i];
+              results.push({
+                url: item.images.standard_resolution.url,
+                message: item.caption.text + '. ' + item.tags.join(', '),
+                user: 'https://api.instagram.com/v1/users/' + item.user.id,
+                type: item.type === 'image'? 'photo' : '',
+                timestamp: item.created_time
+              });
+            }
+          }
           var output = {
-            result: response,
+            result: results,
             source: currentService
           };
           sendResults(output, currentService, requestId);
@@ -190,8 +246,7 @@ function search(req, res, next) {
           response += chunk;
         });
         reply.on('end', function() {      
-          response = JSON.parse(response);
-          //sendResults(response, currentService, requestId);
+          response = JSON.parse(response);          
           var results = [];
           if (response.data && response.data.items) {
             var items = response.data.items;
@@ -199,9 +254,10 @@ function search(req, res, next) {
               var item = items[i];
               results.push({
                 url: item.player.default,
-                message: item.title + ' - ' + item.description,
+                message: item.title + '. ' + item.description,
                 user: 'http://www.youtube.com/' + item.uploader,
-                type: 'video'
+                type: 'video',
+                timestamp: Date.parse(item.uploaded)
               });
             }
           }
@@ -259,23 +315,59 @@ function search(req, res, next) {
             var photos = response.photos.photo;
             for (var i = 0, len = photos.length; i < len; i++) {
               var photo = photos[i];
-              if (photo.ispublic) {
-                results.push({
-                  url: 'http://www.flickr.com/photos/' +
-                      photo.owner + '/' + photo.id + '/',
-                  message: photo.title,
-                  user: 'http://www.flickr.com/photos/' +
-                      photo.owner + '/',
-                  type: (videoSearch? 'video' : 'photo')                          
+              if (photo.ispublic) {                
+                params = {
+                  method: 'flickr.photos.getInfo',
+                  api_key: GLOBAL_config.FLICKR_KEY,
+                  format: 'json',
+                  nojsoncallback: 1,
+                  photo_id: photo.id
+                };
+                params = querystring.stringify(params);
+                options = {
+                  host: 'api.flickr.com',
+                  port: 80,
+                  path: '/services/rest/?' + params,
+                  headers: {
+                    "Accept": "application/json, text/javascript, */*",
+                    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+                    "Accept-Language": "en-US,en;q=0.8,fr-FR;q=0.6,fr;q=0.4,de;q=0.2,de-DE;q=0.2,es;q=0.2,ca;q=0.2",
+                    "Connection": "keep-alive",
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Referer": "http://flickr.com/",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2",
+                    "X-Requested-With": "XMLHttpRequest"
+                  } 
+                };
+                http.get(options, function(reply2) {        
+                  var response2 = '';
+                  reply2.on('data', function(chunk) {
+                    response2 += chunk;
+                  });
+                  reply2.on('end', function() {      
+                    response2 = JSON.parse(response2);                
+                    sendResults(response2, currentService, requestId);                                
+                    results.push({
+                      url: 'http://www.flickr.com/photos/' +
+                          photo.owner + '/' + photo.id + '/',
+                      message: photo.title,
+                      user: 'http://www.flickr.com/photos/' +
+                          photo.owner + '/',
+                      type: (videoSearch? 'video' : 'photo'),
+                      timestamp: Date.parse(response2.photo.dates.taken)                           
+                    });
+                  });
+                }).on('error', function(e) {
+                  // error
                 });
               }
             }
+            var output = {
+              result: results,
+              source: currentService
+            };
+            //sendResults(output, currentService, requestId);                                
           }
-          var output = {
-            result: results,
-            source: currentService
-          };
-          sendResults(output, currentService, requestId);
         });
       }).on('error', function(e) {
         // error
@@ -311,8 +403,21 @@ function search(req, res, next) {
         });
         reply.on('end', function() {      
           response = JSON.parse(response);
+          if (response.results.length) {
+            var items = response.results;
+            var results = [];
+            for (var i = 0, len = items.length; i < len; i++) {
+              var item = items[i];
+              results.push({
+                url: item.post.media.url_full,
+                message: item.post.title + '. ' + item.post.description,
+                user: item.user.url,
+                type: item.post.media.type
+              });
+            }
+          }
           var output = {
-            result: response,
+            result: results,
             source: currentService
           };
           sendResults(output, currentService, requestId);
@@ -320,7 +425,6 @@ function search(req, res, next) {
       }).on('error', function(e) {
         // error
       });
-
     },
     twitpic: function(requestId) {   
       var currentService = 'twitpic';         
@@ -352,11 +456,54 @@ function search(req, res, next) {
         });
         reply.on('end', function() {      
           response = JSON.parse(response);
-          var output = {
-            result: response,
-            source: currentService
-          };
-          sendResults(output, currentService, requestId);
+          var results = [];
+          if (response.length) {
+            for (var i = 0, len = response.length; i < len; i++) {
+              var item = response[i];
+              var id = item.link.replace(/.*?\/(\w+)$/, '$1');
+              params = {
+                id: id
+              };
+              params = querystring.stringify(params);
+              options = {
+                host: 'api.twitpic.com',
+                port: 80,
+                path: '/2/media/show.json?' + params,
+                headers: {
+                  "Accept": "application/json, text/javascript, */*",
+                  "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+                  "Accept-Language": "en-US,en;q=0.8,fr-FR;q=0.6,fr;q=0.4,de;q=0.2,de-DE;q=0.2,es;q=0.2,ca;q=0.2",
+                  "Connection": "keep-alive",
+                  "Content-Type": "application/x-www-form-urlencoded",
+                  "Referer": "http://twitpic.com/search",
+                  "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2",
+                  "X-Requested-With": "XMLHttpRequest"
+                } 
+              };
+              http.get(options, function(reply2) {        
+                var response2 = '';
+                reply2.on('data', function(chunk) {
+                  response2 += chunk;
+                });
+                reply2.on('end', function() {      
+                  response2 = JSON.parse(response2);                  
+                  results.push({
+                    url: item.link,
+                    message: item.message, 
+                    user: 'http://twitter.com/' + response2.user.username,
+                    type: 'photo'
+                  });
+                  var output = {
+                    result: results,
+                    source: currentService
+                  };
+                  sendResults(output, currentService, requestId);                                                                
+                });
+              }).on('error', function(e) {
+                // error
+              });
+            }
+          }
         });
       }).on('error', function(e) {
         // error
